3b7db61dcb5094f540ab6abea5bb9fc29fa9c35f
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-07-24 10:45:24 -0300

    FEAT(auth): setup routes, pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LoginPage, HomePage } from './pages';
+import { Header } from './components';
 
 function App() {
   return (
     <div className="App">
-      <p>Init</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}  exact />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
