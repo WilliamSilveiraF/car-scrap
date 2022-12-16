b49720ff0dee86d483bbe8c22de1becdbad23646
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-12-16 01:33:38 -0300

    feat: set RegisterNewCarPart and SellCarPart

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,37 +1,15 @@
 import './HomePage.css'
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material'
+import { Button } from '@mui/material'
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../context'
-import { axiosAPI } from '../../utils'
-import { thousandMask } from '../../utils/Mask'
-import Web3 from 'web3'
-
-const products = [
-    {"name": "Engine" },
-    {"name": "Transmission" },
-    {"name": "Clutch" },
-    {"name": "Battery" },
-    {"name": "Alternator" },
-    {"name": "Radiator" },
-    {"name": "Axle" },
-    {"name": "Suspension" },
-    {"name": "Brakes" },
-    {"name": "Catalytic Converter" },
-    {"name": "Muffler" },
-    {"name": "Fuel Tank" },
-]
-
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-})
+import RegisterNewCarPart from './components/RegisterNewCarPart'
+import SellCarPart from './components/SellCarPart'
 
 const HomePage = () => {
-    const web3 = new Web3(window.ethereum)
+    const [section_status, set_section_status] = useState('new')
     const [metaMaskAcc, setMetaMaskAcc] = useState('')
     let { user, logoutUser } = useContext(AuthContext)
 
-    const data = require('./data.json')
     useEffect(() => {
         try {
             let provider = window.ethereum;
@@ -42,6 +20,7 @@ const HomePage = () => {
                         setMetaMaskAcc(accounts[0])
                         console.log('accounts', accounts[0])
                     })
+                    .catch(err => console.log(err))
             } else {
                 logoutUser()
                 window.alert('MetaMask is not installed!!!')
@@ -51,86 +30,21 @@ const HomePage = () => {
         }
     }, [])
 
-    const [productData, setProductData] = useState({ 
-        product: 'Engine',
-        car_year: '',
-        price: '', 
-        mileage: '',
-        car_part_base64: ''
-    })
-
-    const order = async () => {
-        try {
-            let newContract = new web3.eth.Contract(data.abi)
-                                .deploy({ data: data.bytecode.object })
-                                .send({ from: metaMaskAcc })
-                                .then(res => {
-                                    console.log('res', res._address)
-                                })
-            console.log(newContract)
-        } catch (err) {
-            console.log('--err', err)
-        }
-    }
-    
-    const handleFile = (event) => {
-        const reader = new FileReader()
-        reader.readAsDataURL(event.target.files[0])
-        reader.onload = event => {
-            setProductData(prev => ({ ...prev, car_part_base64: event.target.result }))
-        }
-    }
-
     if (!metaMaskAcc) {
         return <p>Carregando...</p>
     }
 
+    const components = {
+        'new': <RegisterNewCarPart metaMaskAcc={metaMaskAcc} />,
+        'sell': <SellCarPart metaMaskAcc={metaMaskAcc} />
+    }
+
     return <section className='homePage'>
         <div className='homePage-grid'>
-            <h1>Register a new car part</h1>
-
-            <FormControl variant="standard" >
-                <InputLabel>Part</InputLabel>
-                <Select
-                    id="product"
-                    value={productData?.product}
-                    onChange={event => setProductData(prevData => ({...prevData, product: event.target.value}))}
-                    onBlur={event => setProductData(prevData => ({...prevData, product: event.target.value}))}
-                    label="Tax Structure"
-                >
-                    {
-                        products.map(product => <MenuItem value={product.name}>{ product.name }</MenuItem>)
-                    }
-                </Select>
-            </FormControl>
-            <TextField 
-                id="car_year" 
-                label="Car Year"
-                variant="standard"
-                value={productData.car_year}
-                inputProps={{ maxLength: 4 }}
-                onChange={event => setProductData(prevData => ({ ...prevData, [event.target.id]: event.target.value.replace(/\D/g, '') }))}
-                onBlur={event => setProductData(prevData => ({...prevData, [event.target.id]: event.target.value.replace(/\D/g, '') }))}
-            />
-            <TextField 
-                id="mileage" 
-                label="Mileage"
-                variant="standard"
-                value={productData.mileage}
-                inputProps={{ maxLength: 7 }}
-                onChange={event => setProductData(prevData => ({ ...prevData, [event.target.id]: thousandMask(event.target.value) }))}
-                onBlur={event => setProductData(prevData => ({...prevData, [event.target.id]: thousandMask(event.target.value) }))}
-            />
-            <TextField 
-                id="price" 
-                label="Price" 
-                variant="standard"
-                value={productData.price}
-                onChange={event => setProductData(prevData => ({ ...prevData, [event.target.id]: event.target.value }))}
-                onBlur={event => setProductData(prevData => ({...prevData, [event.target.id]: formatter.format(event.target.value.replace(/\D/g, ''))}))}
-            />
-            <input type='file' onChange={handleFile} />
-            <Button variant="contained" onClick={() => order()}>REGISTER CAR PART</Button>
+            <h1>{section_status === 'new' ? 'Register a new car part' : 'Sell a car part'}</h1>
+            <h2>{ user.username } | Ein { user.ein_number }</h2>
+            <h3><span className={`span_section ${ section_status === 'new' ? 'span_active' : '' }`} onClick={() => set_section_status('new')}>Register</span> | <span className={`span_section ${ section_status === 'sell' ? 'span_active' : '' }`} onClick={() => set_section_status('sell')}>Sell</span></h3>
+            { components[section_status] }
             <hr></hr>
             <Button variant="text" onClick={() => logoutUser()}>Logout</Button>
         </div>
